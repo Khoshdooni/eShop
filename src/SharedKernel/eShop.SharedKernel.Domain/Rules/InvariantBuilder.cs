31690bda3330886e7a271989dd8b3a7dcb2f1748
Khoshdooni <elhamkhoshdooni@gmail.com>
2025-12-11 22:16:20 +0330

    S7_2_min15

---
+++ src/SharedKernel/eShop.SharedKernel.Domain/Rules/InvariantBuilder.cs
@@ -0,0 +1,21 @@
+﻿using eShop.SharedKernel.Domain.Abstractions;
+using eShop.SharedKernel.Domain.Results;
+
+namespace eShop.SharedKernel.Domain.Rules;
+
+public sealed class InvariantBuilder<T>
+    where T : IEntity
+{
+    private readonly T _target;
+    private readonly List<ICheckRule<T>> _rules = new();
+    public InvariantBuilder(T target) => _target = target;
+    public InvariantBuilder<T> Include(ICheckRule<T> rule)
+    {
+        _rules.Add(rule);
+        return this;
+    }
+    public Result EnsureValid()
+    {
+        return Result.Success();
+    }
+}
